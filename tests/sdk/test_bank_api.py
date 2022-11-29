a9f87754f57b01c5e93fcb8c802879918ac10d89
Mark Silva <mark.silva@teseobe.com>
2022-11-29 22:16:37 +0800

    REFACTOR: format files

--- tests/sdk/test_bank_api.py
+++ tests/sdk/test_bank_api.py
@@ -1,7 +1,4 @@
-from pprint import pprint
-
 import obp_python
-from obp_python.rest import ApiException
 
 # Configure API key authorization: directLogin
 configuration = obp_python.Configuration()
@@ -10,16 +7,15 @@ configuration = obp_python.Configuration()
 
 # create an instance of the API class
 api_instance = obp_python.BankApi(obp_python.ApiClient(configuration))
-body = obp_python.EmptyClassJson(
-    ""
-)  # EmptyClassJson | EmptyClassJson object that needs to be added.
+body = obp_python.EmptyClassJson("")
 api_version = "v5.0.0"  # str | eg:v2.2.0, v3.0.0
-# bank_id = 'obp1' # str | The bank id
 bank_id = "Mobilink"  # str | The bank id
 
 
-def test_transaction_types():
-    api_response = api_instance.o_bpv2_0_0_get_transaction_types(body, bank_id).to_dict()
+def test_transaction_types() -> None:
+    api_response = api_instance.o_bpv2_0_0_get_transaction_types(
+        body, bank_id
+    ).to_dict()
     assert type(api_response) is dict
     assert "transaction_types" in api_response
     assert len(api_response["transaction_types"]) == 0
